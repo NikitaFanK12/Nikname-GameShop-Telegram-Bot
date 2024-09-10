2d4a852f9527cfde2b0a5258bff59defeba8d23d
NikitaFanK12 <148260334+NikitaFanK12@users.noreply.github.com>
2024-09-10 17:25:19 +0300

    Add files via upload

---
+++ Telegram Bot/main.py
@@ -0,0 +1,45 @@
+import telebot
+
+bot = telebot.TeleBot("7155044711:AAEfg-1XKr_QxEYq1Md5vFObqVx_a7zYEMU")
+
+@bot.message_handler(content_types=['text'])
+def start(message):
+    text = str()
+    markup = telebot.types.ReplyKeyboardMarkup()
+    if message.text == "/start":
+        text = "Hello, I am a bot"
+        markup.add(telebot.types.KeyboardButton("Магазин"),
+        telebot.types.KeyboardButton("Акції"),
+        telebot.types.KeyboardButton("Новинки"),
+        telebot.types.KeyboardButton("Відгуки"),
+        telebot.types.KeyboardButton("Правила"),
+        telebot.types.KeyboardButton("Тех підтримка"))
+
+        #for i in range(2):
+        #    bot.delete_message(message.chat.id, message.message_id-i)
+    elif message.text == "Магазин":
+        text = "Hello, I am a bot"
+        markup.add(telebot.types.KeyboardButton("Steam"),
+        telebot.types.KeyboardButton("Epic games"),
+        telebot.types.KeyboardButton("Origin"),
+        telebot.types.KeyboardButton("PlayStore"),
+        telebot.types.KeyboardButton("X Box"),
+        telebot.types.KeyboardButton("Black market"),
+        telebot.types.KeyboardButton("Назад"))
+
+        #for i in range(2):
+        #    bot.delete_message(message.chat.id, message.message_id-i)
+    elif message.text == "Назад":
+        text = "Hello, I am a bot"
+        markup.add(telebot.types.KeyboardButton("Магазин"),
+        telebot.types.KeyboardButton("Акції"),
+        telebot.types.KeyboardButton("Новинки"),
+        telebot.types.KeyboardButton("Відгуки"),
+        telebot.types.KeyboardButton("Правила"),
+        telebot.types.KeyboardButton("Тех підтримка"))
+
+        #for i in range(2):
+        #    bot.delete_message(message.chat.id, message.message_id-i)
+    bot.send_message(message.chat.id, text, reply_markup=markup)
+
+bot.infinity_polling()
